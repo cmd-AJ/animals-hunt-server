5410049b92e2e9ad2e82f2b8e9c14454aa0a1d53
cmd-AJ <andrewlol774@gmail.com>
2025-12-20 09:39:16 +0100

    added: backend for animasl

---
+++ src/models/user.model.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    correo: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    telefono: {
+      type: String,
+      required: true,
+    },
+    animal_0: {
+      type: Boolean,
+      default: false,
+    },
+    animal_1: {
+      type: Boolean,
+      default: false,
+    },
+    animal_2: {
+      type: Boolean,
+      default: false,
+    },
+    animal_3: {
+      type: Boolean,
+      default: false,
+    },
+    animal_4: {
+      type: Boolean,
+      default: false,
+    },
+    animal_5: {
+      type: Boolean,
+      default: false,
+    },
+    animal_6: {
+      type: Boolean,
+      default: false,
+    },
+    animal_7: {
+      type: Boolean,
+      default: false,
+    },
+    animal_8: {
+      type: Boolean,
+      default: false,
+    },
+    juegoVecesCompletado: {
+      type: Number,
+      default: 0,
+    },
+    ganador: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true, // Agrega automáticamente `createdAt` y `updatedAt`
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
